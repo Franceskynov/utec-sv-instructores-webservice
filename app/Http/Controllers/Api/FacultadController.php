19ea634e8b9daad86d8b30bfb0b2702426deda16
Franceskynov <franceskynov@yandex.com>
2019-09-23 23:59:50 -0600

    finishing facultad controller

--- app/Http/Controllers/Api/FacultadController.php
+++ app/Http/Controllers/Api/FacultadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Utils\CustomValidators;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Facultad;
@@ -49,22 +50,44 @@ class FacultadController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $validator = CustomValidators::requestValidator($request, CustomValidators::$facultadRules);
+
+        if ($validator->fails())
+        {
+            $this->response = $this->invalidCreation;
+
+        } else {
+
+            Facultad::create($request->all());
+
+            $this->response = $this->successCreation;
+        }
+
+        return \Response::json($this->response);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
-        //
+        if ( $row =  Facultad::find($id)) {
+
+            $this->response = $this->successResponse($row->load('materias'));
+
+        } else {
+
+            $this->response = $this->invalidResponse;
+        }
+
+        return \Response::json($this->response);
     }
 
     /**
@@ -83,21 +106,69 @@ class FacultadController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
-        //
+
+        if($row =  Facultad::find($id))
+        {
+            $rules = CustomValidators::$facultadRules;
+            $rules['abreviacion'] = 'required';
+            $validator = CustomValidators::requestValidator($request, $rules);
+
+            if ($validator->fails())
+            {
+                $this->response = $this->invalidChecking;
+
+            } else {
+
+                if($row->update($request->all()))
+                {
+                    $this->response = $this->successResponse($row);
+
+                } else {
+
+                    $this->response = $this->invalidUpdate;
+                }
+            }
+
+        } else {
+
+            $this->response = $this->notFoundResponse;
+        }
+
+        return \Response::json($this->response);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        if($row = Facultad::find($id))
+        {
+            if ($row->is_enabled)
+            {
+                $row->update([
+                    'is_enabled' => ($row->is_enabled == 1) ? 0 : 0
+                ]);
+
+                $this->response = $this->successDeletion;
+
+            } else {
+
+                $this->response = $this->previouslyDeleted;
+            }
+
+        } else {
+
+            $this->response = $this->notFoundResponse;
+        }
+
+        return \Response::json($this->response);
     }
 }
